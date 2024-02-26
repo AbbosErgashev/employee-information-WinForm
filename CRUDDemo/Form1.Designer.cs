cc849b404688ccc6addf82c59d30faf71b5ed952
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-02-27 03:20:04 +0500

    Add project files.

---
+++ CRUDDemo/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace CRUDDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new System.Windows.Forms.Panel();
+            label2 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            panel2 = new System.Windows.Forms.Panel();
+            btnSearch = new System.Windows.Forms.Button();
+            btnPrint = new System.Windows.Forms.Button();
+            btnDelete = new System.Windows.Forms.Button();
+            btnDisplay = new System.Windows.Forms.Button();
+            btnUpdate = new System.Windows.Forms.Button();
+            btnNew = new System.Windows.Forms.Button();
+            btnAdd = new System.Windows.Forms.Button();
+            label7 = new System.Windows.Forms.Label();
+            textBox4 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label6 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
+            printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.Color.FromArgb(50, 50, 50);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new System.Drawing.Point(-6, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(890, 62);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label2.ForeColor = System.Drawing.Color.White;
+            label2.Location = new System.Drawing.Point(308, 17);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(313, 33);
+            label2.TabIndex = 1;
+            label2.Text = "Employee Information";
+            // 
+            // label1
+            // 
+            label1.Location = new System.Drawing.Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(100, 23);
+            label1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = System.Drawing.Color.FromArgb(32, 32, 32);
+            panel2.Controls.Add(btnSearch);
+            panel2.Controls.Add(btnPrint);
+            panel2.Controls.Add(btnDelete);
+            panel2.Controls.Add(btnDisplay);
+            panel2.Controls.Add(btnUpdate);
+            panel2.Controls.Add(btnNew);
+            panel2.Controls.Add(btnAdd);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Location = new System.Drawing.Point(-6, 62);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(890, 269);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnSearch.ForeColor = System.Drawing.Color.White;
+            btnSearch.Location = new System.Drawing.Point(728, 11);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new System.Drawing.Size(129, 46);
+            btnSearch.TabIndex = 15;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = false;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // btnPrint
+            // 
+            btnPrint.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnPrint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btnPrint.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnPrint.ForeColor = System.Drawing.Color.White;
+            btnPrint.Location = new System.Drawing.Point(731, 155);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new System.Drawing.Size(125, 65);
+            btnPrint.TabIndex = 14;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = false;
+            btnPrint.Click += btnPrint_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btnDelete.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnDelete.ForeColor = System.Drawing.Color.White;
+            btnDelete.Location = new System.Drawing.Point(731, 84);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new System.Drawing.Size(125, 65);
+            btnDelete.TabIndex = 13;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = false;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnDisplay
+            // 
+            btnDisplay.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnDisplay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btnDisplay.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnDisplay.ForeColor = System.Drawing.Color.White;
+            btnDisplay.Location = new System.Drawing.Point(599, 155);
+            btnDisplay.Name = "btnDisplay";
+            btnDisplay.Size = new System.Drawing.Size(125, 65);
+            btnDisplay.TabIndex = 12;
+            btnDisplay.Text = "Display";
+            btnDisplay.UseVisualStyleBackColor = false;
+            btnDisplay.Click += btnDisplay_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btnUpdate.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnUpdate.ForeColor = System.Drawing.Color.White;
+            btnUpdate.Location = new System.Drawing.Point(599, 84);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new System.Drawing.Size(125, 65);
+            btnUpdate.TabIndex = 11;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = false;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnNew
+            // 
+            btnNew.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btnNew.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnNew.ForeColor = System.Drawing.Color.White;
+            btnNew.Location = new System.Drawing.Point(467, 155);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new System.Drawing.Size(125, 65);
+            btnNew.TabIndex = 10;
+            btnNew.Text = "New";
+            btnNew.UseVisualStyleBackColor = false;
+            btnNew.Click += btnNew_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            btnAdd.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnAdd.ForeColor = System.Drawing.Color.White;
+            btnAdd.Location = new System.Drawing.Point(467, 84);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new System.Drawing.Size(125, 65);
+            btnAdd.TabIndex = 9;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label7.ForeColor = System.Drawing.Color.White;
+            label7.Location = new System.Drawing.Point(52, 11);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(198, 25);
+            label7.TabIndex = 8;
+            label7.Text = "Employee information";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            textBox4.ForeColor = System.Drawing.Color.White;
+            textBox4.Location = new System.Drawing.Point(124, 208);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(300, 31);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            textBox3.ForeColor = System.Drawing.Color.White;
+            textBox3.Location = new System.Drawing.Point(124, 161);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(300, 31);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            textBox2.ForeColor = System.Drawing.Color.White;
+            textBox2.Location = new System.Drawing.Point(124, 112);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(300, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = System.Drawing.Color.FromArgb(59, 59, 59);
+            textBox1.ForeColor = System.Drawing.Color.White;
+            textBox1.Location = new System.Drawing.Point(124, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(300, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label6.ForeColor = System.Drawing.Color.White;
+            label6.Location = new System.Drawing.Point(52, 208);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(66, 28);
+            label6.TabIndex = 3;
+            label6.Text = "Salary";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label5.ForeColor = System.Drawing.Color.White;
+            label5.Location = new System.Drawing.Point(52, 161);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(48, 28);
+            label5.TabIndex = 2;
+            label5.Text = "Age";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label4.ForeColor = System.Drawing.Color.White;
+            label4.Location = new System.Drawing.Point(52, 112);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(66, 28);
+            label4.TabIndex = 1;
+            label4.Text = "Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label3.ForeColor = System.Drawing.Color.White;
+            label3.Location = new System.Drawing.Point(52, 66);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(30, 28);
+            label3.TabIndex = 0;
+            label3.Text = "Id";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(40, 40, 40);
+            dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new System.Drawing.Point(-1, 337);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new System.Drawing.Size(900, 320);
+            dataGridView1.TabIndex = 2;
+            // 
+            // printDocument1
+            // 
+            printDocument1.PrintPage += printDocument1_PrintPage;
+            // 
+            // printPreviewDialog1
+            // 
+            printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            printPreviewDialog1.Enabled = true;
+            printPreviewDialog1.Icon = (System.Drawing.Icon)resources.GetObject("printPreviewDialog1.Icon");
+            printPreviewDialog1.Name = "printPreviewDialog1";
+            printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            ClientSize = new System.Drawing.Size(882, 653);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Employee";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
